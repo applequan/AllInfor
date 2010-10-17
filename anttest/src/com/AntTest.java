7960cf512a78ea13e57ddcfaf816f0d0b5b2fa29
applequan <applequan2008@gmail.com>
2010-10-17 16:45:23 +0800

    chan2

---
+++ anttest/src/com/AntTest.java
@@ -0,0 +1,8 @@
+package com;
+public class AntTest{
+       public static void main(String[] args){
+            for(int i=0;i<args.length;i++){
+                  System.out.println(args[i]);
+            }
+       }
+}
