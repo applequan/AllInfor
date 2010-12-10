8f3b958db216d5ce0f638c3e07a73ef2615b6039
applequan <applequan2008@gmail.com>
2010-12-10 10:51:11 +0800

    new structure without older test files]

--- anttest/src/com/AntTest.java
+++
@@ -1,8 +0,0 @@
-package com;
-public class AntTest{
-       public static void main(String[] args){
-            for(int i=0;i<args.length;i++){
-                  System.out.println(args[i]);
-            }
-       }
-}
